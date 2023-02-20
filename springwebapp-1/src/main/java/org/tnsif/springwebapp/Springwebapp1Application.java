a2ce6d70161ce73d0d7c4832aa48fac16e9a793a
P RAJA REDDY <P RAJA REDDY@LAPTOP-N0AS1RU2>
2023-02-20 08:42:11 +0530

    first commit

---
+++ springwebapp-1/src/main/java/org/tnsif/springwebapp/Springwebapp1Application.java
@@ -0,0 +1,15 @@
+package org.tnsif.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springwebapp1Application.class, args);
+		int x=15;
+		System.out.println(x);
+	}
+
+}
---
+++ springwebapp-1/src/main/java/org/tnsif/springwebapp/controller/Hellocontroller.java
@@ -0,0 +1,13 @@
+package org.tnsif.springwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Hellocontroller {
+	
+		public String message()
+	{
+		return "Welcome to B35";
+	}
+
+}
